53a4225af3b766744399075af4abd04fd9ec867a
“Agash912 <agash0912@gmail.com>
2024-04-25 16:45:05 -0400

    profile_details_endpoints

--- fastapi_service/main.py
+++ fastapi_service/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from routers import custom_search, news_loader, profile_updater
+from routers import custom_search, news_loader, profile
 
 app = FastAPI()
 
 # app.include_router(custom_search.router, tags=['search'], prefix='/search')
 app.include_router(news_loader.router, tags=['news'], prefix='/news')
-# app.include_router(profile_updater.router, tags=['profile'], prefix='/profile')
+app.include_router(profile.router, tags=['profile'], prefix='/profile')
